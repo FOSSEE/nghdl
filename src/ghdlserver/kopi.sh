795cbde3f12ed2bd302986e4f5edbe97ebd6bf8a
Raj Mohan <rmohan43@gmail.com>
2017-04-17 20:18:40 +0530

    Removed kopi.sh

--- src/ghdlserver/kopi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp ghdlserver.o ~/ngspice-nghdl/src/xspice/icm/ghdl/myxor/DUTghdl/
-cp ghdlserver.o ~/ngspice-nghdl/src/xspice/icm/ghdl/or_nghdl/DUTghdl/
-cp ghdlserver.o ~/ngspice-nghdl/src/xspice/icm/ghdl/and_nghdl/DUTghdl/
-cp ghdlserver.o ~/ngspice-nghdl/src/xspice/icm/ghdl/counter/DUTghdl/
-cp ghdlserver.o ~/ngspice-nghdl/src/xspice/icm/ghdl/inverter/DUTghdl/
-cp ghdlserver.o ~/ngspice-nghdl/src/xspice/icm/ghdl/flipflop/DUTghdl/
-cp ghdlserver.o ~/ngspice-nghdl/src/xspice/icm/ghdl/fulladder/DUTghdl/
