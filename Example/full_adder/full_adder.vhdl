a81c4ecb38dc18f36688113c6fa1b28a84802183
Rahul Paknikar <rahulp13@users.noreply.github.com>
2019-06-25 09:52:11 +0530

    Update and rename trial_fa.vhdl to full_adder.vhdl

--- Example/full_adder/full_adder.vhdl
+++ Example/full_adder/full_adder.vhdl
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
  
-entity trial_fa is
+entity full_adder is
   port (
     i_bit1  : in std_logic_vector(0 downto 0);
     i_bit2  : in std_logic_vector(0 downto 0);
@@ -10,9 +10,9 @@ entity trial_fa is
     o_sum   : out std_logic_vector(0 downto 0);
     o_carry : out std_logic_vector(0 downto 0)
     );
-end trial_fa;
+end full_adder;
  
-architecture rtl of trial_fa is
+architecture rtl of full_adder is
 begin
   o_sum   <= i_bit1 xor i_bit2 xor i_bit3;
   o_carry <= (i_bit1 and i_bit2) or (i_bit2 and i_bit3) or (i_bit3 and i_bit1);
