eb95026ab9007631eb8e2a1c54dcd38fabcb60ad
saurabhb17 <saurabhbansode@gmail.com>
2019-12-18 15:13:23 +0530

    Examples Restructered

--- Example/combinational_logic/counter/counter.vhdl
+++ Example/combinational_logic/counter/counter.vhdl
@@ -1,23 +1,29 @@
 library ieee;
+
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity up_counter_slv is
+entity counter is
 port(C : in std_logic;
      CLR : in std_logic;
      Q : out std_logic_vector(3 downto 0));
-end up_counter_slv;
+end counter;
 
-architecture bhv of up_counter_slv is
+architecture bhv of counter is
+
 	signal tmp: std_logic_vector(3 downto 0);
 	begin
 	process (C, CLR)
+
 		begin
 			if (CLR='1') then
-				tmp <= "0000";
+			tmp <= "0000";
+
 			elsif (C'event and C='1') then
-				tmp <= std_logic_vector(to_unsigned(1+to_integer(unsigned(tmp)), tmp'length));
+			tmp <= std_logic_vector(to_unsigned(1+to_integer(unsigned(tmp)), tmp'length));
+
 			end if;
+
 	end process;
 	Q <= tmp;
 
